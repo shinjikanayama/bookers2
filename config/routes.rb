66c2034fa646378465c4905b7fb2126471604b97
shinjikanayama <sinzi.1202@gmail.com>
2023-07-18 03:44:49 +0000

    [Finish]headerリンク作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+
+  get 'users/show'
+  get 'users/edit'
+
+  root to: 'homes#top'
+  get "homes/about" => "homes#about", as: "about"
   devise_for :users
+
+  resources :books, only: [:new, :index, :show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
